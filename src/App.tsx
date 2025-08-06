c532972b87ac6cf465e85c1875a63a1549f44783
plingier <philippe_lingier@hotmail.com>
2025-08-06 18:06:44 +0200

    enable for Git pages

--- src/App.tsx
+++ src/App.tsx
@@ -17,12 +17,11 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename={import.meta.env.PROD ? '/bnp-clone' : '/'}>
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/test" element={<Test />} />
             <Route path="/hypothecair-krediet" element={<Mortgage />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
